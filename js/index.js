b4224b9fbf5b5e4ef58884d8b2c0ca3d3411e033
Alicja <k2l8m11n2@gmail.com>
2018-12-01 01:04:58 +0100

    Add dice rolls

--- js/index.js
+++ js/index.js
@@ -1101,6 +1101,27 @@ function parse(msg) {
         .humanize()
     );
   }
+  function getRandomInt(min, max) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
+  }
+  match = m.match(
+    /;([1-9][0-9]|[1-9])?d(100|[1-9][0-9]|[2-9])((?:|\+|-)(?:[1-9][0-9]|[1-9]))?/
+  );
+  if (match) {
+    const dice = parseInt(match[1]) || 1;
+    const sides = parseInt(match[2]);
+    const modifier = parseInt(match[3]) || 0;
+    const rolls = [...Array(dice)]
+      .map(() => getRandomInt(1, sides + 1))
+      .sort((x, y) => x - y);
+    const result = rolls.reduce((x, y) => x + y) + modifier;
+    const modifierDisplay =
+      modifier === 0 ? "" : modifier > 0 ? " + " + modifier : " - " + -modifier;
+    msg.channel.send(`[${rolls.join(", ")}]${modifierDisplay} = ${result}`);
+  }
 }
 
 dclient.login(process.env.VINNY_DISCORD_TOKEN);
