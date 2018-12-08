64cf8347f0e6dd536fbb8f462723cc33d1f90d32
Alicja <k2l8m11n2@gmail.com>
2018-12-08 22:10:11 +0100

    Fix build.sh

--- js/build.sh
+++ js/build.sh
@@ -1,1 +1,1 @@
-sudo docker-compose build --build-arg "COMMIT=$(git rev-parse --short HEAD | tr -d n; test -n "$(git status --porcelain)" && echo ", modified")"
+sudo docker-compose build --build-arg "COMMIT=$(git rev-parse --short HEAD | tr -d \n; test -n "$(git status --porcelain)" && echo ", modified")"
