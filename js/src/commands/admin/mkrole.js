efea051636dbba23446a085cc2012c0e30239fc2
Alicja <k2l8m11n2@gmail.com>
2018-12-22 22:39:23 +0100

    Add role and administrative commands

---
+++ js/src/commands/admin/mkrole.js
@@ -0,0 +1,16 @@
+module.exports = {
+	mkrole: async ({ msg, rawArgs }) => {
+		if (!msg.member.hasPermission("MANAGE_ROLES")) return "no";
+		await msg.guild.createRole({ name: rawArgs });
+		return "Created!";
+	},
+	rmrole: async ({ msg, rawArgs }) => {
+		if (!msg.member.hasPermission("MANAGE_ROLES")) return "no";
+		const role = msg.guild.roles.find(
+			x => x.name.toLowerCase() === rawArgs.toLowerCase()
+		);
+		if (!role) return "Couldn't find that role :/";
+		await role.delete();
+		return "Deleted!";
+	}
+};
