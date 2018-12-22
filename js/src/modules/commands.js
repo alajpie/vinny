af94740dc5d747596979c03f0603a18c21be2dd6
Alicja <k2l8m11n2@gmail.com>
2018-12-22 19:59:39 +0100

    Add command execution

--- js/src/modules/commands.js
+++ js/src/modules/commands.js
@@ -1,6 +1,14 @@
+const glob = require("glob");
+const path = require("path");
 const { debug, info, error, fatal, assert } = require("../logging.js");
 
 const argsRegex = /"(.*?)"|'(.*?)'|(\S+)/g;
+
+const commandPaths = glob.sync(path.join(__dirname, "../commands/*.js"));
+const commands = {};
+commandPaths.forEach(x => {
+	Object.assign(commands, require(path.resolve(x)));
+});
 
 module.exports = {
 	onMessage: function({ config, dclient, msg }) {
@@ -23,6 +31,16 @@ module.exports = {
 					args.push(argsMatch.slice(1).find(x => x));
 				}
 				debug({ command, prefix, args });
+				if (typeof commands[command] === "function") {
+					const result = commands[command](args, {
+						dclient,
+						msg,
+						config: config[command] || {}
+					});
+					if (result) {
+						msg.channel.send(result);
+					}
+				}
 			}
 		});
 	}
