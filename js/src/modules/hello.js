2334c8109cb8bddea16755eb253b1af6f349377b
Autoclave <autoclave@firemail.cc>
2018-12-22 19:39:39 +0100

    Add command parsing (not yet executing)

--- js/src/modules/hello.js
+++ js/src/modules/hello.js
@@ -1,7 +1,7 @@
 const { debug, info, error, fatal, assert } = require("../logging.js");
 
 module.exports = {
-	onMessage: function({ config, dclient, msg }) {
+	onMessage: function({ config, msg }) {
 		if (msg.content.includes("a0673fbf-b2d2-481a-9f26-7823ea7eda08")) {
 			msg.channel.send(`${config.name} says ${config.greeting}!`);
 		}
