30c693108bb77f36b3bf9848fffc32727461d01a
Alicja <k2l8m11n2@gmail.com>
2018-12-25 21:28:34 +0100

    Migrate to better-sqlite3

--- js/src/modules/r5k.js
+++ js/src/modules/r5k.js
@@ -4,40 +4,38 @@ function strip(msg) {
 	return msg.content.toLowerCase().replace(/[^0-9a-z]/g, "");
 }
 
-async function check(db, config, msg, dclient) {
-	if (msg.channel.id !== config.channel) return;
-	if (msg.author.id === dclient.user.id) return;
-	const stripped = strip(msg);
-	const exists = await db.get(
-		"SELECT 1 FROM r5k WHERE message = ?",
-		stripped
-	);
-	debug({ stripped, exists: !!exists });
-	if (exists) {
-		msg.delete(500);
-		dclient.channels
-			.get(config.failChannel)
-			.send(`${msg.author.tag}: ${msg.content}`);
-	} else {
-		await db.run(
-			"INSERT INTO r5k (message) SELECT $m WHERE NOT EXISTS (SELECT 1 FROM r5k WHERE message = $m)",
-			{ $m: stripped }
-		);
-	}
-}
-
 module.exports = {
-	init: async function({ config, db }) {
-		await db.run(
-			"CREATE TABLE IF NOT EXISTS r5k (message TEXT PRIMARY KEY)"
+	init: function({ config, db }) {
+		db.exec("CREATE TABLE IF NOT EXISTS r5k (message TEXT PRIMARY KEY)");
+		const existsPrepared = db.prepare(
+			"SELECT 1 FROM r5k WHERE message = ?"
 		);
+		const insertPrepared = db.prepare(
+			"INSERT INTO r5k (message) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM r5k WHERE message = ?)"
+		);
+		function check(msg, dclient) {
+			if (msg.channel.id !== config.channel) return;
+			if (msg.author.id === dclient.user.id) return;
+			const stripped = strip(msg);
+			const exists = existsPrepared.get(stripped);
+			debug({ stripped, exists: !!exists });
+			if (exists) {
+				msg.delete(500);
+				dclient.channels
+					.get(config.failChannel)
+					.send(`${msg.author.tag}: ${msg.content}`);
+			} else {
+				insertPrepared.run([stripped, stripped]);
+			}
+		}
+
 		return {
 			onMessage: function({ msg, dclient }) {
-				return check(db, config, msg, dclient);
+				return check(msg, dclient);
 			},
 			onEdit: function({ prev, next, dclient }) {
 				if (prev.content === next.content) return;
-				return check(db, config, next, dclient);
+				return check(next, dclient);
 			}
 		};
 	}
