cac82b9e7311a1e92c3d525d9162be5ed1fa1315
Autoclave <autoclave@firemail.cc>
2018-12-26 04:41:45 +0100

    Make separator role non-assignable

--- js/src/commands/iam.js
+++ js/src/commands/iam.js
@@ -15,13 +15,13 @@ module.exports = {
 			debug("has", msg.member.roles.has(role.id));
 			if (add) {
 				if (msg.member.roles.has(role.id)) return "You already were :)";
-				if (role.comparePositionTo(separatorRole) > 0) return "no";
+				if (role.comparePositionTo(separatorRole) >= 0) return "no";
 				await msg.member.addRole(role);
 				return "Added!";
 			} else {
 				if (!msg.member.roles.has(role.id))
 					return "You weren't but now you aren't even more.";
-				if (role.comparePositionTo(separatorRole) > 0)
+				if (role.comparePositionTo(separatorRole) >= 0)
 					return "yes you are";
 				await msg.member.removeRole(role);
 				return "Removed!";
