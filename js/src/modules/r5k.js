ca5a9789f3fb54bd2f582c4f37df4fe10ec9bc4f
Autoclave <autoclave@firemail.cc>
2018-12-26 12:25:27 +0100

    Improve r5k

--- js/src/modules/r5k.js
+++ js/src/modules/r5k.js
@@ -16,20 +16,21 @@ module.exports = {
 			"INSERT INTO r5k (message) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM r5k WHERE message = ?)"
 		);
 		function check(msg, dclient) {
-			if (msg.channel.id !== config.channel) return;
 			if (msg.author.id === dclient.user.id) return;
 			if (msg.type === "PINS_ADD") return;
 			const stripped = strip(msg);
-			const exists = existsPrepared.get(stripped);
-			debug({ stripped, exists: !!exists });
-			if (exists) {
-				msg.delete(500);
-				dclient.channels
-					.get(config.failChannel)
-					.send(`${msg.author.tag}: ${msg.content}`);
-			} else {
-				insertPrepared.run([stripped, stripped]);
-			}
+			db.transaction(() => {
+				const exists = existsPrepared.get(stripped);
+				debug({ stripped, exists: !!exists });
+				if (exists && msg.channel.id === config.channel) {
+					msg.delete(500);
+					dclient.channels
+						.get(config.failChannel)
+						.send(`${msg.author.tag}: ${msg.content}`);
+				} else {
+					insertPrepared.run([stripped, stripped]);
+				}
+			})();
 		}
 
 		return {
