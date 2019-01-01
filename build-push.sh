ba16967be39646553052da5503cc807694465581
Alicja <k2l8m11n2@gmail.com>
2019-01-01 06:29:37 +0100

    Remove Docker and Kubernetes files

--- build-push.sh
+++
@@ -1,5 +0,0 @@
-commit=$(git rev-parse --short HEAD | tr -d "\n"; test -n "$(git status --porcelain)" && echo -n ", modified")
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-str="${commit} (${branch})"
-sudo docker build --build-arg "COMMIT=$str" . -t vinny -t gcr.io/priv-224919/vinny
-sudo docker push gcr.io/priv-224919/vinny
