0cd39c0afb68876fefe0cb44c69fd03ef609ad2c
Autoclave <autoclave@firemail.cc>
2019-01-01 06:29:37 +0100

    Remove Docker and Kubernetes files

--- build.sh
+++
@@ -1,4 +0,0 @@
-commit=$(git rev-parse --short HEAD | tr -d "\n"; test -n "$(git status --porcelain)" && echo -n ", modified")
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-str="${commit} (${branch})"
-sudo docker build --build-arg "COMMIT=$str" . -t vinny
