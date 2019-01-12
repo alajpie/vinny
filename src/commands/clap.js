4ea6203ce5cc82e58437897cfb7def759102cf15
Alicja <k2l8m11n2@gmail.com>
2019-01-12 14:32:44 +0100

    Add a clapwith command

--- src/commands/clap.js
+++ src/commands/clap.js
@@ -1,5 +1,9 @@
 module.exports = {
 	init: () => ({
-		clap: ({ args }) => args.join("👏")
+		clap: ({ args }) => args.join("👏"),
+		clapwith: ({ args }) =>
+			args.length >= 2
+				? args.slice(1).join(args[0])
+				: "Please specify the separator as the first argument."
 	})
 };
