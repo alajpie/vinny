ea64ba8f5c2bcff0f98066f84cc9ac80dc30f20f
Alicja <k2l8m11n2@gmail.com>
2019-01-13 05:51:01 +0100

    Fix display and ranking bugs

--- src/commands/score.js
+++ src/commands/score.js
@@ -1,14 +1,14 @@
 module.exports = {
 	init: ({ db }) => {
 		const scorePrepared = db.prepare(
-			"SELECT sum, RANK() OVER (ORDER BY sum DESC) rank FROM points_sum WHERE serverId = ? AND userId = ?"
+			"SELECT sum, (SELECT COUNT(*) + 1 FROM points_sum WHERE serverId = ps.serverId AND sum > ps.sum) rank FROM points_sum ps WHERE serverId = ? AND userId = ?"
 		);
 		const leaderboardsPrepared = db.prepare(
-			"SELECT RANK() OVER (ORDER BY sum DESC) rank, sum, userId FROM points_sum ORDER BY sum DESC LIMIT 10"
+			"SELECT RANK() OVER (ORDER BY sum DESC) rank, sum, userId FROM points_sum WHERE serverId = ? ORDER BY sum DESC LIMIT 10"
 		);
 		const leaderboards = ({ msg, dclient }) => {
 			let out = "```\n";
-			leaderboardsPrepared.all().forEach(row => {
+			leaderboardsPrepared.all(msg.guild.id).forEach(row => {
 				const user = dclient.users.get(row.userId);
 				const name = user ? user.username : `<@!${row.userId}>`;
 				out += `#${row.rank} ${row.sum.toString().padStart(7, " ")} ${name}\n`;
