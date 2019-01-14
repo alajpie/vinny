03abf801e1bb13897550900cddbeb7839f842a2c
Autoclave <autoclave@firemail.cc>
2019-01-14 19:59:19 +0100

    Add Bandersnatch easter egg

---
+++ src/commands/pac.js
@@ -0,0 +1,6 @@
+module.exports = {
+	init: () => ({
+		pac: () =>
+			`There's messages in every game. Like Pac-Man. Do you know what PAC stands for? P-A-C: "program and control." He's Program and Control Man, the whole thing's a metaphor, he thinks he's got free will but really he's trapped in a maze, in a system, all he can do is consume, he's pursued by demons that are probably just in his own head, and even if he does manage to escape by slipping out one side of the maze, what happens? He comes right back in the other side. People think it's a happy game, it's not a happy game, it's a fucking nightmare world and the worst thing is it's real and we live in it. It's all code. If you listen closely, you can hear the numbers. There's a cosmic flowchart that dictates where you can and where you can't go. I've given you the knowledge. I've set you free. Do you understand?`
+	})
+};
