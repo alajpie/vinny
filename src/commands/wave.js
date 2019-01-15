cbe6c9819672fd0c191b36219a0b37ca03ce342a
Alicja <k2l8m11n2@gmail.com>
2019-01-15 17:42:22 +0100

    Add wave command

---
+++ src/commands/wave.js
@@ -0,0 +1,19 @@
+module.exports = {
+	init: ({ config }) => ({
+		wave: async ({ rawArgs, msg }) => {
+			if (!rawArgs) return;
+			const orig = rawArgs.toLowerCase();
+			const nuMsg = await msg.channel.send(orig); // not "new" because it's a reserved keyword
+			const wave = async (nuMsg, orig, i) => {
+				if (i === orig.length) {
+					return nuMsg.delete();
+				}
+				const nu =
+					orig.substr(0, i) + orig[i].toUpperCase() + orig.substr(i + 1);
+				await nuMsg.edit(nu);
+				setTimeout(() => wave(nuMsg, orig, i + 1), config.delay);
+			};
+			wave(nuMsg, orig, 0);
+		}
+	})
+};
