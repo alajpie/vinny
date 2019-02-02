f2766d7831b037efd27c462ca76e4b86548262c8
Autoclave <autoclave@firemail.cc>
2019-02-02 13:56:13 +0100

    Make stylistic changes

--- src/modules/twitter.js
+++ src/modules/twitter.js
@@ -1,51 +1,53 @@
 const delve = require("dlv");
 const Twit = require("twit");
 
+const re = /\bhttps?:\/\/twitter\.com\/[^\/]+\/status\/([0-9]+)/g;
+
 module.exports = {
-	init: ({secrets}) => {
+	init: ({ secrets }) => {
 		const T = new Twit({
 			consumer_key: secrets.twitterConsumerKey,
 			consumer_secret: secrets.twitterConsumerSecret,
 			app_only_auth: true
 		});
 
-		const re = /\bhttps?:\/\/twitter\.com\/[^\/]+\/status\/([0-9]+)/g;
-
 		return {
-			onMessage: ({ msg }) => {
+			onMessage: async ({ msg }) => {
 				const match = re.exec(msg.content);
 
-				if (match === null) {
+				if (!match) {
 					return;
 				}
 
 				const id = match[1];
 
-				T.get("statuses/show/:id", {
+				const tweet = await T.get("statuses/show/:id", {
 					id,
 					include_entities: true
-				}).then(function(result) {
-					const media = delve(result, "data.extended_entities.media", []);
-					const photos = media.filter(e => e && e.type === "photo");
-					const has_multiple_photos = photos.length > 1;
-
-					const is_thread = delve(result, "data.in_reply_to_user_id", 0) === delve(result, "data.user.id", 1);
-
-					var info = [];
-
-					if (is_thread) {
-						info.push("thread");
-					}
-
-					if (has_multiple_photos) {
-						info.push(`${photos.length - 1} more photos`);
-					}
-
-					if (info.length > 0) {
-						msg.channel.send(`[${info.join(", ")}]`);
-					}
 				});
-			},
+
+				const media = delve(tweet, "data.extended_entities.media", []);
+				const photos = media.filter(e => e && e.type === "photo");
+				const hasMultiplePhotos = photos.length > 1;
+
+				const isThread =
+					delve(tweet, "data.in_reply_to_user_id", 0) ===
+					delve(tweet, "data.user.id", 1);
+
+				let info = [];
+
+				if (isThread) {
+					info.push("thread");
+				}
+
+				if (hasMultiplePhotos) {
+					info.push(`${photos.length - 1} more photos`);
+				}
+
+				if (info.length > 0) {
+					msg.channel.send(`[${info.join(", ")}]`);
+				}
+			}
 		};
 	}
 };
