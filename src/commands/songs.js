9da59b299ae7ca43aa007bb6544eb2c305b035dc
Alicja <k2l8m11n2@gmail.com>
2019-02-19 18:23:58 +0100

    Add Cyboogie to songs

--- src/commands/songs.js
+++ src/commands/songs.js
@@ -1,6 +1,7 @@
 module.exports = {
 	init: () => ({
 		jouvert: () => "https://youtu.be/PkLPFi4fmug",
-		internationale: () => "https://youtu.be/3sh4kz_zhyo"
+		internationale: () => "https://youtu.be/3sh4kz_zhyo",
+		cyboogie: () => "https://youtu.be/_un9PYsE1_g"
 	})
 };
