c4f7ebfc5e5163842c924ec219b483a0bd06e41a
Alicja <k2l8m11n2@gmail.com>
2019-02-21 23:26:47 +0100

    Improve pin checking
    Now it also checks on unReact. Thanks Jess. (:yell:)

--- src/modules/pins.js
+++ src/modules/pins.js
@@ -8,35 +8,38 @@ module.exports = {
 			"INSERT INTO pins (messageId) VALUES (?)"
 		);
 
-		return {
-			onReact: function({ react, user, dclient }) {
-				if (react.message.channel.id === config.channel) return;
-				const pins = new Set();
-				const antipins = new Set();
-				react.message.reactions.forEach(x => {
-					if (config.pins.includes(x.emoji.toString())) {
-						x.users.array().forEach(y => pins.add(y.id));
-					}
-					if (config.antipins.includes(x.emoji.toString())) {
-						x.users.array().forEach(y => antipins.add(y.id));
-					}
-				});
-				pins.delete(react.message.author.id)
-				if (
-					pins.size - antipins.size >= config.threshold &&
-					!existsPrepared.get(react.message.id)
-				) {
-					dclient.channels
-						.get(config.channel)
-						.send(
-							`<@${react.message.author.id}> in <#${
-								react.message.channel.id
-							}>: ${react.message.content}`,
-							{ files: react.message.attachments.array().map(x => x.url) }
-						);
-					insertPrepared.run(react.message.id);
+		function check({ react, user, dclient }) {
+			if (react.message.channel.id === config.channel) return;
+			const pins = new Set();
+			const antipins = new Set();
+			react.message.reactions.forEach(x => {
+				if (config.pins.includes(x.emoji.toString())) {
+					x.users.array().forEach(y => pins.add(y.id));
 				}
+				if (config.antipins.includes(x.emoji.toString())) {
+					x.users.array().forEach(y => antipins.add(y.id));
+				}
+			});
+			pins.delete(react.message.author.id);
+			if (
+				pins.size - antipins.size >= config.threshold &&
+				!existsPrepared.get(react.message.id)
+			) {
+				dclient.channels
+					.get(config.channel)
+					.send(
+						`<@${react.message.author.id}> in <#${react.message.channel.id}>: ${
+							react.message.content
+						}`,
+						{ files: react.message.attachments.array().map(x => x.url) }
+					);
+				insertPrepared.run(react.message.id);
 			}
+		}
+
+		return {
+			onReact: check,
+			onUnreact: check
 		};
 	}
 };
