0338144108830dc93939d09c7d448163491e9848
Alicja <k2l8m11n2@gmail.com>
2019-02-27 23:18:41 +0100

    Fix help message for the coin command

--- src/commands/random.js
+++ src/commands/random.js
@@ -3,5 +3,6 @@ const uuid = require("uuid/v4");
 const coin = () => (Math.random() < 0.5 ? "Heads!" : "Tails!");
 
 module.exports = {
+	commands: { ulid: null, uuid: null, coin: ["flip"] },
 	init: () => ({ coin, flip: coin, ulid: () => ulid(), uuid })
 };
