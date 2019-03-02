6ee3cbac1af8b90213dd7658aec4ebddf9b24410
Alicja <k2l8m11n2@gmail.com>
2019-03-02 13:17:35 +0100

    Add emojis module

---
+++ src/modules/emojis.js
@@ -0,0 +1,23 @@
+require("string.prototype.matchall").shim();
+
+module.exports = {
+	init: function({ config }) {
+		return {
+			onMessage: function({ msg, dclient }) {
+				if (msg.author.id === dclient.user.id) return;
+				for (const match of msg.content.toLowerCase().matchAll(/:([^ :]+):/g)) {
+					for (const serverId of config.servers) {
+						const guild = dclient.guilds.get(serverId);
+						const emoji = guild.emojis.find(
+							x => x.name.toLowerCase() === match[1]
+						);
+						if (emoji) {
+							msg.channel.send(emoji.toString());
+							break;
+						}
+					}
+				}
+			}
+		};
+	}
+};
