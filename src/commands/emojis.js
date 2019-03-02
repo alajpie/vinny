098828f534e593c20d663e6e5482298bc751e626
Alicja <k2l8m11n2@gmail.com>
2019-03-02 14:02:14 +0100

    Add emojis command

---
+++ src/commands/emojis.js
@@ -0,0 +1,21 @@
+const _ = require("lodash");
+
+module.exports = {
+	init: () => ({
+		emojis: ({ msg, dclient, globalConfig }) => {
+			const mojis = [];
+			console.log(globalConfig);
+			for (const serverId of _.get(
+				globalConfig,
+				["servers", msg.guild.id, "moduleConfig", "emojis", "servers"],
+				[]
+			)) {
+				const guild = dclient.guilds.get(serverId);
+				for (const emoji of guild.emojis.array()) {
+					mojis.push(emoji.toString());
+				}
+			}
+			return mojis.join(" ");
+		}
+	})
+};
