b44b372106675b2780bae35e1e876a2b6d7da98f
Alicja <k2l8m11n2@gmail.com>
2019-03-21 20:52:54 +0100

    Fix UTC module formatting again
    Because I think I did it the other way around accidentally.

--- src/modules/utc.js
+++ src/modules/utc.js
@@ -1,18 +1,25 @@
 module.exports = {
-  init: function({config, db, serverId}) {
-    return {
-      onReady: function({dclient, serverId}) {
-	function timer() {
-	  const date = new Date();
-	  const utc = date.getUTCHours() + ':' +
-	      date.getUTCMinutes().toString().padStart(2, '0') + ' UTC';
-	  dclient.guilds.get(serverId)
-	      .members.get(dclient.user.id)
-	      .setNickname(utc);
-	  setTimeout(timer, 60000 - (new Date() % 60000));  // next full minute
+	init: function({ config, db, serverId }) {
+		return {
+			onReady: function({ dclient, serverId }) {
+				function timer() {
+					const date = new Date();
+					const utc =
+						date.getUTCHours() +
+						":" +
+						date
+							.getUTCMinutes()
+							.toString()
+							.padStart(2, "0") +
+						" UTC";
+					dclient.guilds
+						.get(serverId)
+						.members.get(dclient.user.id)
+						.setNickname(utc);
+					setTimeout(timer, 60000 - (new Date() % 60000)); // next full minute
+				}
+				timer();
+			}
+		};
 	}
-	timer();
-      }
-    };
-  }
 };
