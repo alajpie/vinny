77c8cb2b03a7fe88942d71b94c84ef4ba007d81e
Alicja <k2l8m11n2@gmail.com>
2019-03-26 23:50:06 +0100

    Add restricted emojis to the emojis command

--- src/commands/emojis.js
+++ src/commands/emojis.js
@@ -4,11 +4,39 @@ module.exports = {
 	init: () => ({
 		emojis: ({ msg, dclient, globalConfig }) => {
 			const mojis = [];
-			for (const serverId of _.get(
+			let servers = _.get(
 				globalConfig,
 				["servers", msg.guild.id, "moduleConfig", "emojis", "servers"],
 				[]
-			)) {
+			);
+			const restrictedChannels = _.get(
+				globalConfig,
+				[
+					"servers",
+					msg.guild.id,
+					"moduleConfig",
+					"emojis",
+					"restrictedChannels"
+				],
+				[]
+			);
+			const restrictedServers = _.get(
+				globalConfig,
+				[
+					"servers",
+					msg.guild.id,
+					"moduleConfig",
+					"emojis",
+					"restrictedServers"
+				],
+				[]
+			);
+
+			if (restrictedChannels.includes(msg.channel.id)) {
+				servers = [...servers, ...restrictedServers];
+			}
+
+			for (const serverId of servers) {
 				const guild = dclient.guilds.get(serverId);
 				for (const emoji of guild.emojis.array()) {
 					mojis.push(emoji.toString());
